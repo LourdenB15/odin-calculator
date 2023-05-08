e0ffcab3f09578975922e7ec22867557c13caa93
LourdenB15 <lourdenbaydal15@gmail.com>
2023-05-08 17:45:36 +0800

    Fix negative number issue & refactor code

--- app.js
+++ app.js
@@ -1,201 +1,178 @@
 const digits = document.querySelectorAll('.digits');
-const extra = document.querySelectorAll('.extra');
-const operations = document.querySelectorAll('.operations');
-const input = document.querySelector('.input-value');
-const result = document.querySelector('.result');
+const operation = document.querySelectorAll('.operation');
+const input = document.querySelector('#input');
+const result = document.querySelector('#result');
 
-let x = 0;                                  // variables
-let operator = '';
-let y = 0;
-let aa = '';
-let answer;
+const period = document.querySelector('.period');
+const remove = document.querySelector('.delete');
+const ac = document.querySelector('.ac');
+const equal = document.querySelector('.equal');
 
-const remove = document.querySelector('.delete'); // delete selector
-remove.addEventListener('click', () => {          // delete event listener
-    input.textContent = input.textContent.slice(0, input.textContent.length - 1)
-})
+let x;
+let operator;
+let y;
+let total;
 
+let operationClicked = true;
+let numberClicked = false;
+let dotClicked = false;
 
-const equal = document.querySelector('.equal');             //eaual selector
-equal.addEventListener('click', () => {                     //equal event listener
-    aa = '';
-    if (input.textContent.includes('+')) {
-        if (input.textContent.includes('+') && input.textContent.indexOf('+') === input.textContent.length - 1) {
-            return;
-        } else if (input.textContent.includes('+')) {
-            operator = '+';
-            [x, y] = input.textContent.split('+')
-            x = parseFloat(x);
-            y = parseFloat(y);
-            answer = Math.round(operate(x, y, operator) * 100) / 100;
-            result.textContent = answer;
-            x = answer;
-            input.textContent = x;
-            y = 0;
-        }
-    } else if (input.textContent.includes('-')) {
-        if (input.textContent.includes('-') && input.textContent.indexOf('-') === input.textContent.length - 1) {
-            return;
-        } else if (input.textContent.includes('-')) {
-            operator = '-';
-            [x, y] = input.textContent.split('-')
-            x = parseFloat(x);
-            y = parseFloat(y);
-            answer = Math.round(operate(x, y, operator) * 100) / 100;
-            result.textContent = answer;
-            x = answer;
-            input.textContent = x;
-            y = 0;
-        }
-    } else if (input.textContent.includes('x')) {
-        if (input.textContent.includes('x') && input.textContent.indexOf('x') === input.textContent.length - 1) {
-            return;
-        } else if (input.textContent.includes('x')) {
-            operator = 'x';
-            [x, y] = input.textContent.split('x')
-            x = parseFloat(x);
-            y = parseFloat(y);
-            answer = operate(x, y, operator);
-            result.textContent = answer;
-            x = answer;
-            input.textContent = x;
-            y = 0;
-        }
-    } else if (input.textContent.includes('÷')) {
-        if (input.textContent.includes('÷') && input.textContent.indexOf('÷') === input.textContent.length - 1) {
-            return;
-        } else if (input.textContent.includes('÷')) {
-            operator = '÷';
-            [x, y] = input.textContent.split('÷')
-            x = parseFloat(x);
-            y = parseFloat(y);
-            answer = operate(x, y, operator);
-            result.textContent = answer;
-            x = answer;
-            input.textContent = x;
-            y = 0;
-        }
-    }
-})
+const checkOperator = /[\x+\-\/]/g;
+const checkNumber = /[0-9]/;
 
-const ac = document.querySelector('.ac');   // for CLEAR
-ac.addEventListener('click', () => {       // clear Event listener
-    x = 0;                                  // variables
-    operator = '';
-    y = 0;
-    aa = '';
-    answer = 0;
+ac.addEventListener('click', allClear);
+function allClear() {
+    x = undefined;
+    operator = undefined;
+    y = undefined;
+    total = undefined;
+    operationClicked = true;
+    numberClicked = false;
+    dotClicked = false;
+    result.textContent = '0';
     input.textContent = '';
-    result.textContent = answer;
-})
-
-digits.forEach(digit => {                   // for the DIGITS
-    digit.addEventListener('click', displayValue);
-})
-
-function displayValue(e) {                  // Display function
-    input.textContent += e.target.textContent;
-    aa += e.target.textContent;
 }
 
-const period = document.querySelector('.period'); // period selector
-period.addEventListener('click', (e) => {         //periodeventlistener
-    if (!aa.includes('.')) displayValue(e)
-})
-
-const plus = document.querySelector('.plus');     //plus selector
-plus.addEventListener('click', (e) => {           //plus event listener
-    aa = '';
-    if ((input.textContent.includes('+') || input.textContent.includes('-') || input.textContent.includes('x') || input.textContent.includes('÷')) && (input.textContent.indexOf('+') === input.textContent.length - 1 || input.textContent.indexOf('-') === input.textContent.length - 1 || input.textContent.indexOf('÷') === input.textContent.length - 1 || input.textContent.indexOf('x') === input.textContent.length - 1)) {
-        return;
-    } else if (input.textContent.includes('+') || input.textContent.includes('-') || input.textContent.includes('x') || input.textContent.includes('÷')) {
-        if (input.textContent.includes('+')) operator = '+';
-        if (input.textContent.includes('-')) operator = '-';
-        if (input.textContent.includes('x')) operator = 'x';
-        if (input.textContent.includes('÷')) operator = '÷';
-        [x, y] = input.textContent.split(operator)
-        x = parseFloat(x);
-        y = parseFloat(y);
-        answer = Math.round(operate(x, y, operator) * 100) / 100;
-        result.textContent = answer;
-        x = answer;
-        input.textContent = x;
-        y = 0;
-
+remove.addEventListener('click', deleteCalc);
+function deleteCalc() {
+    const deleted = input.textContent.slice(input.textContent.length - 1);
+    if (deleted === '+' || deleted === '-' || deleted === 'x' || deleted === '/') {
+        if (input.textContent.slice(0, input.textContent.length - 1).includes('.')) {
+            dotClicked = true;
+        } else {
+            dotClicked = false;
+        }
+        operationClicked = false;
+        numberClicked = true;
+        deleteChar();
+    } else if (deleted === '.') {
+        dotClicked = false;
+        deleteChar();
     }
-    input.textContent += e.target.textContent
-});
-const minus = document.querySelector('.minus');     //minus selector
-minus.addEventListener('click', (e) => {           //minus event listener
-    aa = '';
-    if ((input.textContent.includes('+') || input.textContent.includes('-') || input.textContent.includes('x') || input.textContent.includes('÷')) && (input.textContent.indexOf('+') === input.textContent.length - 1 || input.textContent.indexOf('-') === input.textContent.length - 1 || input.textContent.indexOf('÷') === input.textContent.length - 1 || input.textContent.indexOf('x') === input.textContent.length - 1)) {
-        return;
-    } else if (input.textContent.includes('+') || input.textContent.includes('-') || input.textContent.includes('x') || input.textContent.includes('÷')) {
-        if (input.textContent.includes('+')) operator = '+';
-        if (input.textContent.includes('-')) operator = '-';
-        if (input.textContent.includes('x')) operator = 'x';
-        if (input.textContent.includes('÷')) operator = '÷';
-        [x, y] = input.textContent.split(operator)
-        x = parseFloat(x);
-        y = parseFloat(y);
-        answer = Math.round(operate(x, y, operator) * 100) / 100;
-        result.textContent = answer;
-        x = answer;
-        input.textContent = x;
-        y = 0;
+    else {
+        deleteChar();
+    }
+}
+function deleteChar() {
+    if (input.textContent.slice(0, input.textContent.length - 1) === '') {
+        operationClicked = true;
+        numberClicked = false;
+        dotClicked = false;
+    }
+    input.textContent = input.textContent.slice(0, input.textContent.length - 1);
+}
 
-    }
-    input.textContent += e.target.textContent
-});
-const times = document.querySelector('.times');     //minus selector
-times.addEventListener('click', (e) => {           //minus event listener
-    aa = '';
-    if ((input.textContent.includes('+') || input.textContent.includes('-') || input.textContent.includes('x') || input.textContent.includes('÷')) && (input.textContent.indexOf('+') === input.textContent.length - 1 || input.textContent.indexOf('-') === input.textContent.length - 1 || input.textContent.indexOf('÷') === input.textContent.length - 1 || input.textContent.indexOf('x') === input.textContent.length - 1)) {
-        return;
-    } else if (input.textContent.includes('+') || input.textContent.includes('-') || input.textContent.includes('x') || input.textContent.includes('÷')) {
-        if (input.textContent.includes('+')) operator = '+';
-        if (input.textContent.includes('-')) operator = '-';
-        if (input.textContent.includes('x')) operator = 'x';
-        if (input.textContent.includes('÷')) operator = '÷';
-        [x, y] = input.textContent.split(operator)
-        x = parseFloat(x);
-        y = parseFloat(y);
-        answer = Math.round(operate(x, y, operator) * 100) / 100;
-        result.textContent = answer;
-        x = answer;
-        input.textContent = x;
-        y = 0;
+period.addEventListener('click', display);
 
+digits.forEach(digit => digit.addEventListener('click', display));
+
+operation.forEach(e => e.addEventListener('click', display));
+
+function display() {
+    if (input.textContent.length < 35) {
+        if (checkNumber.test(this.textContent)) {
+            operationClicked = false;
+            numberClicked = true;
+            input.textContent += this.textContent;
+        }
+        if ((this.textContent === '+' || this.textContent === '-' || this.textContent === 'x' || this.textContent === '/') && !operationClicked && numberClicked) {
+            operationCalc(this);
+        }
+        if (this.textContent === '.' && !dotClicked && numberClicked) {
+            dotClicked = true;
+            numberClicked = false;
+            input.textContent += this.textContent;
+        }
     }
-    input.textContent += e.target.textContent
-});
-const divide = document.querySelector('.divide');     //divide selector
-divide.addEventListener('click', (e) => {           //divide event listener
-    aa = '';
-    if ((input.textContent.includes('+') || input.textContent.includes('-') || input.textContent.includes('x') || input.textContent.includes('÷')) && (input.textContent.indexOf('+') === input.textContent.length - 1 || input.textContent.indexOf('-') === input.textContent.length - 1 || input.textContent.indexOf('÷') === input.textContent.length - 1 || input.textContent.indexOf('x') === input.textContent.length - 1)) {
-        return;
-    } else if (input.textContent.includes('+') || input.textContent.includes('-') || input.textContent.includes('x') || input.textContent.includes('÷')) {
-        if (input.textContent.includes('+')) operator = '+';
-        if (input.textContent.includes('-')) operator = '-';
-        if (input.textContent.includes('x')) operator = 'x';
-        if (input.textContent.includes('÷')) operator = '÷';
-        [x, y] = input.textContent.split(operator)
-        x = parseFloat(x);
-        y = parseFloat(y);
-        answer = Math.round(operate(x, y, operator) * 100) / 100;
-        result.textContent = answer;
-        x = answer;
-        input.textContent = x;
-        y = 0;
-    }
-    input.textContent += e.target.textContent
+}
+
+equal.addEventListener('click', () => {
+    answerChecker();
+    assignOperator();
+    operate(operator);
+    x = parseFloat(total);
+    y = '';
+    operator = '';
+    result.textContent = total;
+    input.textContent = `${total}`;
 });
 
-function operate(x, y, operator) {
-    if (operator === 'x') return multiply(x, y);
-    if (operator === '÷') return division(x, y);
-    if (operator === '-') return substract(x, y);
-    if (operator === '+') return add(x, y);
+function assignOperator() {
+    let sign = input.textContent.slice(x.length, input.textContent.length - y.length);
+    switch (sign) {
+        case '+':
+            operator = '+';
+            break;
+        case '-':
+            operator = '-';
+            break;
+        case 'x':
+            operator = 'x';
+            break;
+        case '/':
+            operator = '/';
+            break;
+        default:
+            operator = '';
+    }
+}
+
+function calcFormula(e) {
+    assignOperator();
+    x = parseFloat(x);
+    y = parseFloat(y);
+    operate(operator);
+    x = total.toString();
+    y = '';
+    result.textContent = total;
+    operator = e.textContent;
+    input.textContent = `${total}${operator}`;
+}
+
+function operationCalc(e) {
+    answerChecker();
+    if (input.textContent.split(checkOperator).length === 2 && input.textContent.split(checkOperator)[0] !== '') {
+        calcFormula(e);
+    } else if (input.textContent.split(checkOperator).length === 3) {
+        calcFormula(e);
+    } else {
+        operator = e.textContent;
+        input.textContent += operator;
+    }
+    operationClicked = true;
+    dotClicked = false;
+    numberClicked = false;
+}
+
+function answerChecker() {
+    let numVar;
+    if (Math.sign(result.textContent) === -1) {
+        numVar = input.textContent.split(checkOperator);
+        numVar.shift();
+        x = '-' + numVar[0];
+        y = numVar[1];
+    } else {
+        [x, y] = input.textContent.split(checkOperator);
+    }
+}
+
+function operate(operator) {
+    switch (operator) {
+        case '+':
+            total = Math.round(add(x, y) * 100) / 100;
+            break;
+        case '-':
+            total = Math.round(substract(x, y) * 100) / 100;
+            break;
+        case 'x':
+            total = Math.round(multiply(x, y) * 100) / 100;
+            break;
+        case '/':
+            total = Math.round(division(x, y) * 100) / 100;
+            break;
+        default:
+            total = 'ERROR';
+    }
 }
 
 const add = function (x, y) {
@@ -213,5 +190,3 @@ const multiply = function (x, y) {
 const division = function (x, y) {
     return x / y;
 }
-
-
